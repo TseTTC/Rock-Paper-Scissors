e01eb9eb004c2133972b2839f262dacb8ac2f012
TseTTC <tsettcv@gmail.com>
2021-03-31 23:55:07 +0800

    Added working JS + CSS

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 --First commit--
 
-RPS.js
--Added computerPlay() function to generate computer's RPS
--Added playRound() function to be return who wins
--Added Test code
+RPS.js\
+-Added computerPlay() function to generate computer's RPS\
+-Added playRound() function to be return who wins\
+-Added Test code\
--- RPS.js
+++ RPS.js
@@ -1,8 +1,8 @@
 function computerPlay() {
     x = Math.floor(Math.random() * 3)
-    console.log(x)
     if (x == 0){
         return("Rock")
+        document.getElementById("enemyPaper").click();
     } else if (x == 1){
         return("Paper")
     } else {
@@ -10,35 +10,89 @@ function computerPlay() {
     }
 }
 
+let playerScore = 0;
+let enemyScore = 0;
+
 function playRound(playerSelection,computerSelection) {
-    if (playerSelection = "Rock"){
-        if(computerSelection = "Rock"){
+    if (playerSelection == "Rock"){
+        if(computerSelection == "Rock"){
+            console.log(enemyScore)
+            console.log(playerScore)
+            document.getElementById("playerScore1").innerHTML = playerScore;
+            document.getElementById("enemyScore1").innerHTML = enemyScore;
             return("Draw! Rock vs Rock")
-        } else if (computerSelection = "Paper"){
+        } else if (computerSelection == "Paper"){
+            enemyScore++
+            console.log(enemyScore)
+            console.log(playerScore)
+            document.getElementById("playerScore1").innerHTML = playerScore;
+            document.getElementById("enemyScore1").innerHTML = enemyScore;
             return("You lose! Rock vs Paper, Paper beats Rock")
         } else {
+            playerScore++
+            console.log(enemyScore)
+            console.log(playerScore)
+            document.getElementById("playerScore1").innerHTML = playerScore;
+            document.getElementById("enemyScore1").innerHTML = enemyScore;
             return("You win! Rock vs Scissors, Rock beats Scissors")
+            
         }
-    } else if (playerSelection = "Paper"){
-        if(computerSelection = "Paper"){
+    } else if (playerSelection == "Paper"){
+        if(computerSelection == "Paper"){
             return("Draw! Paper vs Paper")
-        }else if(computerSelection = "Scissors"){
+        }else if(computerSelection == "Scissors"){
+            enemyScore++
+            console.log(enemyScore)
+            console.log(playerScore)
+            document.getElementById("playerScore1").innerHTML = playerScore;
+            document.getElementById("enemyScore1").innerHTML = enemyScore;
             return("You lose! Paper vs Scissors, Scissors beats Paper")
         }else{
+            playerScore++
+            console.log(enemyScore)
+            console.log(playerScore)
+            document.getElementById("playerScore1").innerHTML = playerScore;
+            document.getElementById("enemyScore1").innerHTML = enemyScore;
             return("You win! Paper vs Rock, Paper beats Rock")
+            
         }
-    } else {
-        if(computerSelection = "Scissors"){
+    } else if (playerSelection == "Scissors") {
+        if(computerSelection == "Scissors"){
             return("Draw! Scissors vs Scissors")
-        } else if (computerSelection = "Rock"){
+        } else if (computerSelection == "Rock"){
+            enemyScore++
+            console.log(enemyScore)
+            console.log(playerScore)
+            document.getElementById("playerScore1").innerHTML = playerScore;
+            document.getElementById("enemyScore1").innerHTML = enemyScore;
             return("You lose! Scissors vs Rock, Rock beats Scissors")
+            
         } else {
+            playerScore++
+            console.log(enemyScore)
+            console.log(playerScore)
+            document.getElementById("playerScore1").innerHTML = playerScore;
+            document.getElementById("enemyScore1").innerHTML = enemyScore;
             return("You win! Scissors vs Paper, Scissors beats Paper")
+            
         }
     }
+
 }
-//Test code
-  const playerSelection = "rock";
-  const computerSelection = computerPlay();
-  console.log(playRound(playerSelection, computerSelection));
+
+function game(val){
+    let i;
+    for (i = 0; i < 5 ; i++){
+        let playerSelection = val
+        let computerSelection = computerPlay();
+        console.log(playRound(playerSelection, computerSelection));
+    }
+}
+
+function clicks(val){
+    let playerSelection = val
+    let computerSelection = computerPlay();
+    console.log(playRound(playerSelection, computerSelection));
+}
+
 
